8950a724191bfd8314c67bbf091324bdc5d67134
AlbertoF17 <albertofergom@gmail.com>
2023-03-09 13:57:35 +0100

    Portfolio

--- AlbertoFernandezGomez_Portfolio/js/ejercicio3.js
+++ AlbertoFernandezGomez_Portfolio/js/ejercicio3.js
@@ -1,20 +1,80 @@
 // Obtener el input del asunto
-const subjectInput = document.querySelector('label[for=subject]');
+const asunto = document.querySelector('#subject');
+const correo = document.querySelector('#cc');
+const mensaje = document.querySelector('#body');
 
 // Agregar un escucha de evento para cuando el input pierde el focus
-subjectInput.addEventListener('blur', () => {
+asunto.addEventListener('blur', () => {
   // Verificar si el input está vacío
-  if (subjectInput.value.trim() === '') {
+  if (asunto.value.trim() === '') {
     // Cambiar el borde del input a rojo
-    subjectInput.style.border = '2px solid red';
+    asunto.style.border = '2px solid red';
 
-    // Agregar un mensaje de error debajo del campo
-    const errorText = document.createElement('span');
+    // Agregar un mensaje de error debajo del campo si no existe uno
+    let errorText = asunto.parentNode.querySelector('span');
+    if (!errorText) {
+      errorText = document.createElement('span');
+      errorText.style.color = 'red';
+      asunto.parentNode.appendChild(errorText);
+    }
     errorText.innerText = 'Este campo no puede estar vacío';
-    errorText.style.color = 'red';
-    subjectInput.parentNode.appendChild(errorText);
   } else {
     // Si el input es válido, cambiar el borde a verde
-    subjectInput.style.border = '2px solid green';
+    asunto.style.border = '2px solid green';
+    const errorText = asunto.parentNode.querySelector('span');
+    if (errorText) {
+      errorText.remove();
+    }
+  }
+});
+
+correo.addEventListener('blur', () => {
+  // Verificar si el input está vacío o no es un correo electrónico válido
+  const ccRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+  if (correo.value.trim() === '' || !ccRegex.test(correo.value.trim())) {
+    // Cambiar el borde del input a rojo
+    correo.style.border = '2px solid red';
+
+    // Agregar un mensaje de error debajo del campo si no existe uno
+    let errorText = correo.parentNode.querySelector('span');
+    if (!errorText) {
+      errorText = document.createElement('span');
+      errorText.style.color = 'red';
+      correo.parentNode.appendChild(errorText);
+    }
+    errorText.innerText = 'Debe introducir un correo electrónico válido';
+  } else {
+    // Si el input es válido, cambiar el borde a verde
+    correo.style.border = '2px solid green';
+
+    // Eliminar cualquier mensaje de error previo
+    const errorText = correo.parentNode.querySelector('span');
+    if (errorText) {
+      errorText.remove();
+    }
+  }
+});
+
+mensaje.addEventListener('blur', () => {
+  // Verificar si el input está vacío
+  if (mensaje.value.trim() === '') {
+    // Cambiar el borde del input a rojo
+    mensaje.style.border = '2px solid red';
+
+    // Agregar un mensaje de error debajo del campo si no existe uno
+    let errorText = mensaje.parentNode.querySelector('span');
+    if (!errorText) {
+      errorText = document.createElement('span');
+      errorText.style.color = 'red';
+      mensaje.parentNode.appendChild(errorText);
+    }
+    errorText.innerText = 'Este campo no puede estar vacío';
+  } else {
+    // Si el input es válido, cambiar el borde a verde
+    mensaje.style.border = '';
+    const errorText = mensaje.parentNode.querySelector('span');
+    if (errorText) {
+      errorText.remove();
+    }
   }
 });
