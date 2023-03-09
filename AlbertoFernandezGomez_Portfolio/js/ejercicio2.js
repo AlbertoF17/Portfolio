944d4ea66089470371bc140bc389d6e45e75cfa5
AlbertoF17 <albertofergom@gmail.com>
2023-03-09 13:33:08 +0100

    Portfolio

--- AlbertoFernandezGomez_Portfolio/js/ejercicio2.js
+++ AlbertoFernandezGomez_Portfolio/js/ejercicio2.js
@@ -1,16 +1,29 @@
 const botones = document.querySelectorAll('.tecnologia');
+const cartas = document.querySelectorAll('.carta');
+
 botones.forEach((boton) => {
-  boton.onclick = (evento) => {
-    const tecnologiaSeleccionada = evento.target.getAttribute('data-tecnologia');
-    const cartas = document.querySelectorAll('.carta');
-    for (const carta of cartas) {
-      if (carta.querySelector(`.${tecnologiaSeleccionada}`)) {
-        carta.style.visibility = 'visible';
-      } else if (`${tecnologiaSeleccionada}` == 'todas') {
-        carta.style.visibility = 'visible';
-      } else {
-        carta.style.visibility = 'hidden';
-      }
-    }
-  };
+    boton.onclick = async (evento) => {
+        const tecnologiaSeleccionada = boton.getAttribute('data-tecnologia');
+        for (const carta of cartas) {
+            if (carta.querySelector(`.${tecnologiaSeleccionada}`) || tecnologiaSeleccionada === 'todas') {
+                carta.classList.remove('oculta');
+            } else {
+                carta.classList.add('oculta');
+            }
+        }
+        await sleep(200);
+        for (const carta of cartas) {
+            if (carta.classList.contains('oculta')) {
+                carta.style.display = 'none';
+            } else {
+                carta.style.display = 'flex';
+            }
+        }
+    };
 });
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//Animación de aparición demasiado brusca
